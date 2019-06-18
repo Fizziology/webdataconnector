d5b4bafbd8c0de9c838db55b20bb629fabd067af
Cheryl Schaefer <CherylSchaefer@gmail.com>
2019-06-18 10:10:00 -0500

    updated server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,5 @@ set :keep_releases, 4
 set :rails_env, "amazon"
 set :rack_env, "amazon"
 
-server '52.91.163.132', roles: %w{app web}, ssh_options: { user: 'deploy', forward_agent: true }
+#server '52.91.163.132', roles: %w{app web}, ssh_options: { user: 'deploy', forward_agent: true }
+server '54.211.3.191', roles: %w{app web}, ssh_options: { user: 'deploy', forward_agent: true }
